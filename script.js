9e72435226858d3382a5cdc14f6c804bc644a7c8
bellamalo1 <bmaldo23@gmail.com>
2022-06-23 10:05:11 -0700

    welcome function complete

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("you got this!")
+//cached elements 
+const welcomeButtom = document.querySelector("#welcomebtn")
+const welcomeMessageContainer = document.querySelector("#Welcome-page-container")
+const homepageView = document.querySelector("#Home-page-view")
+//functions 
+function welcome (){
+   console.log('hello')
+   welcomeMessageContainer.classList.add('hide')
+   homepageView.classList.remove('hide')
+   
+}
+
+// attached event listeners
+welcomeButtom.addEventListener('click',welcome)
