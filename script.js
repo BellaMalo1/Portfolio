4a78ca4c01eabce2d078eb7292bd10d368c268a2
bellamalo1 <bmaldo23@gmail.com>
2022-06-24 16:50:28 -0700

    links to social media

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const welcomeButtom = document.querySelector("#welcomebtn")
 const welcomeMessageContainer = document.querySelector("#Welcome-page-container")
 const homepageView = document.querySelector("#Home-page-view")
 const aboutMe = document.querySelector("#about-me-container")
+const bodyFooter = document.querySelector("#social-links")
 //functions 
 function welcome (){
    console.log('hello')
    welcomeMessageContainer.classList.add('hide')
    homepageView.classList.remove('hide')
    aboutMe.classList.remove('hide')
-   
+   bodyFooter.classList.remove('hide')
 
    
 }
